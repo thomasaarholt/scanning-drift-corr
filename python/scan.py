14c441734c6aeb0f7aae7f681998c70bd8c374fb
Thomas Aarholt <thomasaarholt@gmail.com>
2019-07-11 02:16:38 +0200

    nearly complete linear correction

--- python/scan.py
+++ python/scan.py
@@ -1,3 +1,4 @@
+import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.ndimage import gaussian_filter
@@ -5,7 +6,9 @@ from scipy.signal import convolve2d
 from scipy.stats import gaussian_kde
 import scipy.stats as st
 from dataclasses import dataclass
+from scipy.ndimage.morphology import distance_transform_edt
 
+from tqdm.auto import tqdm
 
 def rot_matrix(theta):
     'Thanks Hamish'
@@ -43,7 +46,26 @@ class sMergeClass:
     pass
 
 
+def linear_drift(sMerge):
+    for a0 in tqdm(range(len(sMerge.linearSearch))):
+        for a1 in tqdm(range(len(sMerge.linearSearch))):
+            xyShift = [sMerge.inds*sMerge.xDrift[a0, a1], sMerge.inds*sMerge.yDrift[a0, a1]]
+
+            sMerge.scanOr[:2] += np.tile(xyShift, [2, 1, 1])
+            sMerge = SPmakeImage(sMerge, 0)
+            sMerge = SPmakeImage(sMerge, 1)
+
+            m = np.fft.fft2(
+                sMerge.w2*sMerge.imageTransform[0]
+                ) * np.conj(
+                sMerge.w2*sMerge.imageTransform[1])
+            Icorr = np.fft.ifft2(np.sqrt(abs(m))*np.exp(1j*np.angle(m))).real
+            sMerge.linearSearchScore1[a0, a1] = np.max(Icorr)
+            sMerge.scanOr[:2] -= np.tile(xyShift, [2, 1, 1])
+    return sMerge
+
 def SPmerge01(data, scanAngles):
+    skip = False
     import numpy as np
 
     sMerge = sMergeClass()
@@ -87,9 +109,9 @@ def SPmerge01(data, scanAngles):
         sMerge.scanDir[a0] = [cosd(theta + 90), sind(theta + 90)]
 
     sMerge.linearSearch = sMerge.linearSearch * sMerge.scanLines.shape[1]
-    yDrift, xDrift = np.meshgrid(sMerge.linearSearch, sMerge.linearSearch)
-    sMerge.linearSearchScore1 = np.zeros(len(sMerge.linearSearch))
-    inds = np.linspace(-0.5, 0.5, sMerge.scanLines.shape[1]).T
+    sMerge.yDrift, sMerge.xDrift = np.meshgrid(sMerge.linearSearch, sMerge.linearSearch)
+    sMerge.linearSearchScore1 = np.zeros((len(sMerge.linearSearch), len(sMerge.linearSearch)))
+    sMerge.inds = np.linspace(-0.5, 0.5, sMerge.scanLines.shape[1]).T
 
     N = sMerge.scanLines.shape
 
@@ -101,21 +123,89 @@ def SPmerge01(data, scanAngles):
         ((0, pada), (0, padb)),
         mode='constant', constant_values=0)
 
-    w2 = np.roll(paddedarray, np.round(padamount/2).astype(int))
+    sMerge.w2 = np.roll(paddedarray, np.round(padamount/2).astype(int))
 
-    for a0 in range(len(sMerge.linearSearch)):
-        for a1 in range(len(sMerge.linearSearch)):
-            xyShift = [inds*xDrift[a0, a1], inds*yDrift[a0, a1]]
+# First linear
 
-            sMerge.scanOr[:2] += np.tile(xyShift, [2, 1, 1])
-            SPmakeImage(sMerge, 0)
-            break
-        break
+    if not skip:
+        sMerge = linear_drift(sMerge)
 
 
-def SPmakeImage(sMerge, indImage=0):
+    # Second linear
+        ind = np.argmax(sMerge.linearSearchScore1.flatten())
+        xInd, yInd = np.unravel_index(ind, sMerge.linearSearchScore1.shape)
+
+        step = sMerge.linearSearch[1] - sMerge.linearSearch[0]
+        xRefine = sMerge.linearSearch[xInd] + np.linspace(-0.5, 0.5, len(sMerge.linearSearch))*step
+        yRefine = sMerge.linearSearch[yInd] + np.linspace(-0.5, 0.5, len(sMerge.linearSearch))*step
+
+        sMerge.yDrift, sMerge.xDrift = np.meshgrid(yRefine, xRefine)
+        sMerge.linearSearchScore2 = np.zeros((len(sMerge.linearSearch), len(sMerge.linearSearch)))
+
+        sMerge = linear_drift(sMerge)
+
+
+    with open('savedstate.pickle', 'rb') as handle:
+        sMerge = pickle.load(handle)['sMerge']
+
+    ind = np.argmax(sMerge.linearSearchScore1.flatten())
+    xInd, yInd = np.unravel_index(ind, sMerge.linearSearchScore2.shape)
+    sMerge.xyLinearDrift = np.array([sMerge.xDrift[xInd,0], sMerge.yDrift[yInd,0]])
+
+    # Apply linear shift
+    xyShift = [sMerge.inds*sMerge.xyLinearDrift[0], sMerge.inds*sMerge.xyLinearDrift[1]]
+
+    for a0 in range(sMerge.numImages):
+        sMerge.scanOr[a0] = sMerge.scanOr[a0] + xyShift
+    sMerge = SPmakeImage(sMerge, a0)
+    dxy = np.zeros((sMerge.numImages, 2))
+
+    G1 = np.fft.fft2(sMerge.w2*sMerge.imageTransform[0])
+    for a0 in range(1, sMerge.numImages):
+        G2 = np.fft.fft2(sMerge.w2*sMerge.imageTransform[a0])
+        m = G1*np.conj(G2)
+        Icorr = np.fft.ifft2(np.sqrt(abs(m))*np.exp(1j*np.angle(m))).real
+        ind = np.argmax(Icorr.flatten())
+
+        dx, dy = np.unravel_index(ind, Icorr.shape)
+
+        # Might need (Icorr.shape[0] - 1)/2
+        dx = dx - 1 + Icorr.shape[0]/2 % Icorr.shape[0] - Icorr.shape[0]/2
+        dy = dy - 1 + Icorr.shape[1]/2 % Icorr.shape[1] - Icorr.shape[1]/2
+        dxy[a0] = dxy[a0-1] + np.array([dx, dy])
+        G1 = G2
+
+    dxy[:, 0] -= np.mean(dxy[:, 0])
+    dxy[:, 1] -= np.mean(dxy[:, 1])
+    # Apply alignments and regenerate images
+    for a0 in range(sMerge.numImages):
+        # error on a0 = 1
+
+        sMerge.scanOr[a0, 0] += dxy[a0, 0]
+        sMerge.scanOr[a0, 1] += dxy[a0, 1]
+        if a0 == 1:
+            sMerge = SPmakeImage(sMerge, a0, True)
+        else:
+            sMerge = SPmakeImage(sMerge, a0)
+
+    sMerge.ref = np.round(sMerge.imageSize/2).astype(int)
+    imagePlot = np.mean(sMerge.imageTransform, axis=0)
+    dens = np.prod(sMerge.imageDensity, 0)
+    # Scale intensity of image
+    mask = dens > 0.5
+    imagePlot = imagePlot - np.mean(imagePlot[mask])
+    imagePlot = imagePlot / np.sqrt(np.mean(imagePlot[mask]**2))
+
+    plt.figure()
+    plt.imshow(imagePlot)
+    plt.show()
+    return sMerge
+
+
+def SPmakeImage(sMerge, indImage=0, debug=False):
     indLines = np.ones(sMerge.scanLines.shape[2], dtype=bool)
 
+    # Unclear if arange in t = should be 0 to +0 or not
     t = np.tile(
         np.arange(1, sMerge.scanLines.shape[2]+1), reps=[indLines.sum(), 1])
     xtile = sMerge.scanOr[indImage, 0, indLines]
@@ -138,6 +228,8 @@ def SPmakeImage(sMerge, indImage=0):
     dy = yInd-yIndF
     w1 = np.array([(1-dx)*(1-dy), dx*(1-dy), (1-dx)*dy, dx*dy])
 
+    if debug:
+        breakpoint()
     indAll = np.ravel_multi_index((xAll, yAll), sMerge.imageSize)
     sL = sMerge.scanLines[indImage, :, indLines].T
 
@@ -147,6 +239,7 @@ def SPmakeImage(sMerge, indImage=0):
         weights=weights,
         minlength=np.prod(sMerge.imageSize))
 
+    weights = w1.flatten()
     acc2 = np.bincount(
         indAll.flatten(), weights=weights, minlength=np.prod(sMerge.imageSize))
 
@@ -168,10 +261,18 @@ def SPmakeImage(sMerge, indImage=0):
     sig[sub] = sig[sub] / count[sub]
     sMerge.imageTransform[indImage] = sig
 
-    # plt.figure()
-    # plt.imshow(sig)
-    # plt.show()
+    plt.figure()
+    plt.imshow(sig)
+    plt.show()
 
+    bound = (count == 0).astype(int)
+    bound[[0, -1]] = 1
+    bound[:, [0, -1]] = 1
+    euclid = distance_transform_edt(1-bound)
+    euclidian_min = np.minimum(
+        euclid/sMerge.edgeWidth, 1)
+    sMerge.imageDensity[indImage] = np.sin(euclidian_min*np.pi/2)**2
+    return sMerge
 
 #     % Apply KDE
 #     r = max(ceil(sMerge.sMerge.KDEsigma*3),5);

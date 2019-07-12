00575031c87b6aedf0cea1c3e419eaa4aaa88d6f
Thomas Aarholt <thomasaarholt@gmail.com>
2019-07-12 18:46:54 +0200

    wip

--- python/scan.py
+++ python/scan.py
@@ -1,5 +1,6 @@
 import pickle
 import numpy as np
+from scipy import signal
 import matplotlib.pyplot as plt
 from scipy.ndimage import gaussian_filter
 from scipy.signal import convolve2d
@@ -9,6 +10,16 @@ from dataclasses import dataclass
 from scipy.ndimage.morphology import distance_transform_edt
 
 from tqdm.auto import tqdm
+
+
+def gaussian_filter_fft(img, kernel):
+    fourier = np.fft.fft2(img) * np.fft.fft2(kernel, img.shape)
+    return np.fft.ifft2(fourier).real
+
+
+def gaussian_filter_opencv(img, n, std):
+    return cv2.GaussianBlur(src=img, ksize=(n, n), sigmaX=std, sigmaY=std)
+
 
 def rot_matrix(theta):
     'Thanks Hamish'
@@ -27,29 +38,72 @@ def cosd(deg):
 
 
 def hanningLocal(N):
-    N = N
     Dim1Window = np.sin(np.pi*np.arange(1, N+1)/(N+1))**2
     Dim2Window = Dim1Window[:, None]
     return Dim2Window
 
 
-def gkern(kernlen=21, nsig=3):
-    """Returns a 2D Gaussian kernel."""
-    lim = kernlen//2 + (kernlen % 2)/2
-    x = np.linspace(-lim, lim, kernlen+1)
-    kern1d = np.diff(st.norm.cdf(x))
-    kern2d = np.outer(kern1d, kern1d)
-    return kern2d/kern2d.sum()
+def gaussian_kernel(n, std, normalised=False):
+    '''
+    Generates a n x n matrix with a centered gaussian
+    of standard deviation std centered on it. If normalised,
+    its volume equals 1.'''
+    gaussian1D = signal.gaussian(n, std)
+    gaussian2D = np.outer(gaussian1D, gaussian1D)
+    if normalised:
+        gaussian2D /= (2*np.pi*(std**2))
+    return gaussian2D
 
 
+@dataclass
 class sMergeClass:
-    pass
+    '''Python dataclass containing the registration data
+    scanLines and scanAngles should both be lists or numpy arrays
+    '''
+    scanLines: np.ndarray
+    scanAngles: np.ndarray
+    paddingScale: float = (1+1/4)
+    KDEsigma: float = 1/2  # - Smoothing between pixels for KDE.
+    # - size of edge blending relative to input images.
+    edgeWidth: float = 1/128
+    # Initial linear search vector, relative to image size.
+    linearSearch: np.ndarray = np.linspace(-0.02, 0.02, 1+2*2)
+
+    def __post_init__(self):
+        shape = np.array(self.scanLines.shape)
+        self.shape = shape
+
+        self.imageSize = np.round(shape[1:]*self.paddingScale/4).astype(int)*4
+        self.numImages = shape[0]
+        self.scanOr = np.zeros([self.numImages, 2, shape[1]])
+        self.scanDir = np.zeros([self.numImages, 2])
+
+        padded_shape = (self.numImages,) + tuple(self.imageSize)
+        self.imageTransform = np.zeros(padded_shape)
+        self.imageDensity = np.zeros(padded_shape)
+
+        self.calculate_directions()
+
+    def calculate_directions(self):
+        shape = self.shape
+        for a0 in range(self.numImages):
+            theta = self.scanAngles[a0]
+            xy = np.stack([np.arange(0, shape[1]), np.ones(shape[1])])
+            xy -= (shape[1:]/2)[:, None]
+
+            xy = rot_matrix(theta) @ xy
+
+            xy += (self.imageSize/2)[:, None]
+            xy -= (xy[:, 0] % 1)[:, None]
+            self.scanOr[a0] = xy
+            self.scanDir[a0] = [cosd(theta + 90), sind(theta + 90)]
 
 
 def linear_drift(sMerge):
     for a0 in tqdm(range(len(sMerge.linearSearch))):
         for a1 in tqdm(range(len(sMerge.linearSearch))):
-            xyShift = [sMerge.inds*sMerge.xDrift[a0, a1], sMerge.inds*sMerge.yDrift[a0, a1]]
+            xyShift = [sMerge.inds*sMerge.xDrift[a0, a1],
+                       sMerge.inds*sMerge.yDrift[a0, a1]]
 
             sMerge.scanOr[:2] += np.tile(xyShift, [2, 1, 1])
             sMerge = SPmakeImage(sMerge, 0)
@@ -57,66 +111,34 @@ def linear_drift(sMerge):
 
             m = np.fft.fft2(
                 sMerge.w2*sMerge.imageTransform[0]
-                ) * np.conj(
+            ) * np.conj(
                 sMerge.w2*sMerge.imageTransform[1])
             Icorr = np.fft.ifft2(np.sqrt(abs(m))*np.exp(1j*np.angle(m))).real
             sMerge.linearSearchScore1[a0, a1] = np.max(Icorr)
             sMerge.scanOr[:2] -= np.tile(xyShift, [2, 1, 1])
     return sMerge
 
+
 def SPmerge01(data, scanAngles):
     skip = False
     import numpy as np
 
-    sMerge = sMergeClass()
-
-    paddingScale = (1+1/4)
-    sMerge.KDEsigma = 1/2  # - Smoothing between pixels for KDE.
-    # - size of edge blending relative to input images.
-    sMerge.edgeWidth = 1/128
-    # Initial linear search vector, relative to image size.
-    sMerge.linearSearch = np.linspace(-0.02, 0.02, 1+2*2)
-
-    # I have changed the convention here so that the number of images is the first axis, not the last
-    # I am only supporting the case where the image input is a list / array of images
-
     data = np.array(data)
-    shape = np.array(data.shape)
+    scanAngles = np.array(scanAngles)
 
-    sMerge.scanAngles = np.array(scanAngles)
-    sMerge.imageSize = np.round(shape[1:]*paddingScale/4).astype(int)*4
-    sMerge.numImages = len(sMerge.scanAngles)
-    sMerge.scanLines = np.zeros(shape)
-    sMerge.scanOr = np.zeros([sMerge.numImages, 2, shape[1]])
-    sMerge.scanDir = np.zeros([sMerge.numImages, 2])
-
-    padded_shape = (sMerge.numImages,) + tuple(sMerge.imageSize)
-    sMerge.imageTransform = np.zeros(padded_shape)
-    sMerge.imageDensity = np.zeros(padded_shape)
-
-    sMerge.scanLines[:] = data
-
-    for a0 in range(sMerge.numImages):
-        theta = sMerge.scanAngles[a0]
-        xy = np.stack([np.arange(0, shape[1]), np.ones(shape[1])])
-        xy -= (shape[1:]/2)[:, None]
-
-        xy = rot_matrix(theta) @ xy
-
-        xy += (sMerge.imageSize/2)[:, None]
-        xy -= (xy[:, 0] % 1)[:, None]
-        sMerge.scanOr[a0] = xy
-        sMerge.scanDir[a0] = [cosd(theta + 90), sind(theta + 90)]
+    sMerge = sMergeClass(data, scanAngles)
 
     sMerge.linearSearch = sMerge.linearSearch * sMerge.scanLines.shape[1]
-    sMerge.yDrift, sMerge.xDrift = np.meshgrid(sMerge.linearSearch, sMerge.linearSearch)
-    sMerge.linearSearchScore1 = np.zeros((len(sMerge.linearSearch), len(sMerge.linearSearch)))
+    sMerge.yDrift, sMerge.xDrift = np.meshgrid(
+        sMerge.linearSearch, sMerge.linearSearch)
+    sMerge.linearSearchScore1 = np.zeros(
+        (len(sMerge.linearSearch), len(sMerge.linearSearch)))
     sMerge.inds = np.linspace(-0.5, 0.5, sMerge.scanLines.shape[1]).T
 
-    N = sMerge.scanLines.shape
+    _, xpx, ypx = sMerge.shape
 
-    TwoDHanningWindow = hanningLocal(N[1])*hanningLocal(N[2]).T
-    padamount = sMerge.imageSize - N[1:]
+    TwoDHanningWindow = hanningLocal(xpx)*hanningLocal(ypx).T
+    padamount = sMerge.imageSize - sMerge.shape[1:]
     pada, padb = padamount
     paddedarray = np.pad(
         TwoDHanningWindow,
@@ -130,30 +152,34 @@ def SPmerge01(data, scanAngles):
     if not skip:
         sMerge = linear_drift(sMerge)
 
-
     # Second linear
         ind = np.argmax(sMerge.linearSearchScore1.flatten())
         xInd, yInd = np.unravel_index(ind, sMerge.linearSearchScore1.shape)
 
         step = sMerge.linearSearch[1] - sMerge.linearSearch[0]
-        xRefine = sMerge.linearSearch[xInd] + np.linspace(-0.5, 0.5, len(sMerge.linearSearch))*step
-        yRefine = sMerge.linearSearch[yInd] + np.linspace(-0.5, 0.5, len(sMerge.linearSearch))*step
+        xRefine = sMerge.linearSearch[xInd] + \
+            np.linspace(-0.5, 0.5, len(sMerge.linearSearch))*step
+        yRefine = sMerge.linearSearch[yInd] + \
+            np.linspace(-0.5, 0.5, len(sMerge.linearSearch))*step
 
         sMerge.yDrift, sMerge.xDrift = np.meshgrid(yRefine, xRefine)
-        sMerge.linearSearchScore2 = np.zeros((len(sMerge.linearSearch), len(sMerge.linearSearch)))
+        sMerge.linearSearchScore2 = np.zeros(
+            (len(sMerge.linearSearch), len(sMerge.linearSearch)))
 
         sMerge = linear_drift(sMerge)
 
-
-    with open('savedstate.pickle', 'rb') as handle:
-        sMerge = pickle.load(handle)['sMerge']
+    else:
+        with open('savedstate.pickle', 'rb') as handle:
+            sMerge = pickle.load(handle)['sMerge']
 
     ind = np.argmax(sMerge.linearSearchScore1.flatten())
     xInd, yInd = np.unravel_index(ind, sMerge.linearSearchScore2.shape)
-    sMerge.xyLinearDrift = np.array([sMerge.xDrift[xInd,0], sMerge.yDrift[yInd,0]])
+    sMerge.xyLinearDrift = np.array(
+        [sMerge.xDrift[xInd, 0], sMerge.yDrift[yInd, 0]])
 
     # Apply linear shift
-    xyShift = [sMerge.inds*sMerge.xyLinearDrift[0], sMerge.inds*sMerge.xyLinearDrift[1]]
+    xyShift = [sMerge.inds*sMerge.xyLinearDrift[0],
+               sMerge.inds*sMerge.xyLinearDrift[1]]
 
     for a0 in range(sMerge.numImages):
         sMerge.scanOr[a0] = sMerge.scanOr[a0] + xyShift
@@ -184,7 +210,7 @@ def SPmerge01(data, scanAngles):
         sMerge.scanOr[a0, 0] += dxy[a0, 0]
         sMerge.scanOr[a0, 1] += dxy[a0, 1]
         if a0 == 1:
-            sMerge = SPmakeImage(sMerge, a0, True)
+            sMerge = SPmakeImage(sMerge, a0, debug=False)
         else:
             sMerge = SPmakeImage(sMerge, a0)
 
@@ -220,6 +246,8 @@ def SPmakeImage(sMerge, indImage=0, debug=False):
     xInd2 = np.clip(xInd, 0, sMerge.imageSize[0]-1)
     yInd2 = np.clip(yInd, 0, sMerge.imageSize[1]-1)
 
+    # Bilinear interpolation on the image in a 2x2 pixel fashion¨
+    # xAll and yAll are the coordinates of each pixel, w1 the intensity
     xIndF = np.floor(xInd2).astype(int)
     yIndF = np.floor(yInd2).astype(int)
     xAll = np.array([xIndF, xIndF+1, xIndF, xIndF+1])
@@ -230,6 +258,7 @@ def SPmakeImage(sMerge, indImage=0, debug=False):
 
     if debug:
         breakpoint()
+    # Get the 2D indices of the bilinear interpolation
     indAll = np.ravel_multi_index((xAll, yAll), sMerge.imageSize)
     sL = sMerge.scanLines[indImage, :, indLines].T
 
@@ -250,20 +279,14 @@ def SPmakeImage(sMerge, indImage=0, debug=False):
     # plt.show()
 
     r = np.maximum(np.ceil(sMerge.KDEsigma*3), 5.0)
+    kern = gkern(2*r+1, sMerge.KDEsigma)
     # Just do gaussian smoothing instead?
-    # sig = gaussian_filter(sig, 2)
-    # count = gaussian_filter(count, sMerge.KDEsigma)
-    kern = gkern(11, 0.5)
-    sig = convolve2d(sig, kern, 'same')
-    count = convolve2d(count, kern, 'same')
+    sig = gaussian_filter(sig, sMerge.KDEsigma)
+    count = gaussian_filter(count, sMerge.KDEsigma)
 
     sub = count > 0
     sig[sub] = sig[sub] / count[sub]
     sMerge.imageTransform[indImage] = sig
-
-    plt.figure()
-    plt.imshow(sig)
-    plt.show()
 
     bound = (count == 0).astype(int)
     bound[[0, -1]] = 1

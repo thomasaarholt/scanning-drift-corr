6b44668b4e50cf0371cb6519dfece72bb0fbd688
Thomas Aarholt <thomasaarholt@gmail.com>
2019-07-30 01:51:22 +0200

    added some comments, may be possible to eliminate a large chunk with scipy rotate

--- python/makeimage_new.py
+++ python/makeimage_new.py
@@ -8,10 +8,14 @@ def SPmakeImage(ss, indImage=0, indLines=None, debug=False):
     if indLines is None:
         indLines = np.full(ss.scanLines.shape[2], True)
 
+    # When we have non-90-deg images, the pixels must be interpolated onto a square grid.
+    # This is done by bilinear interpolation
+    # POSSIBLY: Replace the entire block to "TO HERE" with scipy.ndimage.rotate with order=1 for bilinear
+    # interpolation
+
     # Unclear if arange in t = should be 0 to +0 or not
     # Edit: Apparently, it needs to be 2 to +2 in order to match matlab
     tt = np.arange(ss.scanLines.shape[2])
-
     xInd = (
         ss.scanOr[indImage, 0, indLines][:, None] + tt * ss.scanDir[indImage, 0]
     ).flatten()
@@ -19,21 +23,24 @@ def SPmakeImage(ss, indImage=0, indLines=None, debug=False):
         ss.scanOr[indImage, 1, indLines][:, None] + tt * ss.scanDir[indImage, 1]
     ).flatten()
 
+    breakpoint()
+
     xInd2 = np.clip(xInd, 0, ss.imageSize[0] - 1)  # end with -1)?
     yInd2 = np.clip(yInd, 0, ss.imageSize[1] - 1)
 
     # Bilinear interpolation on the image in a 2x2 pixel fashion¨
     # xAll and yAll are the coordinates of each pixel, w1 the intensity
-    xIndF = np.floor(xInd2).astype(int)
-    yIndF = np.floor(yInd2).astype(int)
+    # Astype int also floors array
+    xIndF = xInd2.astype(int)
+    yIndF = yInd2.astype(int)
     xAll = np.array([xIndF, xIndF + 1, xIndF, xIndF + 1])
-
     yAll = np.array([yIndF, yIndF, yIndF + 1, yIndF + 1])
     with open("results.txt", "a") as f:
         f.write(str(xAll.max(1) + 1))
         f.write(" ")
         f.write(str(yAll.max(1) + 1))
         f.write("\n")
+
     dx = xInd - xIndF
     dy = yInd - yIndF
     w1 = np.array([(1 - dx) * (1 - dy), dx * (1 - dy), (1 - dx) * dy, dx * dy])
@@ -54,6 +61,10 @@ def SPmakeImage(ss, indImage=0, indLines=None, debug=False):
     )
 
     sig = np.reshape(acc, ss.imageSize)
+
+    plt.figure()
+    plt.imshow(sig)
+    plt.show()
     count = np.reshape(acc2, ss.imageSize)
 
     r = np.maximum(np.ceil(ss.KDEsigma * 3), 5.0)
@@ -63,6 +74,8 @@ def SPmakeImage(ss, indImage=0, indLines=None, debug=False):
 
     sub = count > 0
     sig[sub] = sig[sub] / count[sub]
+
+    # TO HERE
     ss.imageTransform[indImage] = sig
     # plt.figure()
     # plt.imshow(sig)
--- python/scan.py
+++ python/scan.py
@@ -76,7 +76,7 @@ def linear_drift(ss):
 def SPmerge01(data, scanAngles):
     with open("results.txt", "w+") as f:
         f.write("")
-    skip = True
+    skip = False
     import numpy as np
 
     data = np.array(data)
@@ -155,6 +155,12 @@ def SPmerge01(data, scanAngles):
         img2_fft = np.fft.fft2(ss.weights * img2)
         Icorr = cross_correlation(img1_fft, img2_fft)
         dx, dy = np.unravel_index(Icorr.argmax(), Icorr.shape)
+
+        print("")
+        print(dx, dy)
+        from skimage.feature import register_translation
+
+        print(register_translation(img1, img2))
 
         # Might need (Icorr.shape[0] - 1)/2
         dx2 = (dx - 1 + Icorr.shape[0] / 2) % Icorr.shape[0] - Icorr.shape[0] / 2

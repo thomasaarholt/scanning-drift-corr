47a4b8d9129830eceed7c90456f4f9195fec2b0f
Thomas Aarholt <thomasaarholt@gmail.com>
2019-08-30 09:42:45 +0200

    wip need to fix transform of single shear

--- python/fast.py
+++ python/fast.py
@@ -1,38 +1,69 @@
 import numpy as np
 from numpy.fft import fft2
-
 import matplotlib.pyplot as plt
+
+from pathlib import Path
 import h5py
-from skimage.transform import rotate
-from scipy.ndimage import gaussian_filter, fourier_shift
-from ipywidgets import widgets as w
-from IPython.display import display
+from scipy.ndimage import fourier_shift
 
 from transforms import (
+    hybrid_correlation,
+    phase_correlation,
     cross_correlation,
-    translation,
-    translation_orig,
+    phase_translation,
+    cross_translation,
     pad_images,
+    set_shear_and_scale_ranges,
+    set_transform_matrices,
     transform,
+    transform_single_image,
     normalise_max,
-    setsize,
-    is_notebook,
 )
 
-file = "../data_examples/nonlinear_drift_correction_synthetic_dataset_for_testing.mat"
-f = h5py.File(file, mode="r")
-img1 = f["image00deg"][:]
-img2 = f["image90deg"][:]
-img3 = f["imageIdeal"][:]
-images = [img1, img2]
-angles = [0, 90]
+pad_scale = 1.1
+steps = 5
+correlation_method = "cross"
+shear_steps = steps
+scale_steps = steps
 
-from scipy.misc import face, ascent
-from scipy.ndimage import rotate, gaussian_filter
+if correlation_method == "phase":
+    correlation_function = phase_correlation
+    translation_function = phase_translation
+elif correlation_method == "hybrid":
+    correlation_function = phase_correlation
+    translation_function = phase_translation
+elif correlation_method == "cross":
+    correlation_function = cross_correlation
+    translation_function = cross_translation
+else:
+    raise ValueError("No correlation method with that name")
 
-if True:
+test_dataset = "C"
+
+if test_dataset == "A":
+    import hyperspy.api as hs
+
+    def get_haadf(slist):
+        "Helper function for picking out HAADF from velox format"
+        for s in slist:
+            if "HAADF" in s.metadata.General.title:
+                return s
+
+    folder = r"C:\Users\Me\Documents\STEM Images"
+    signals = [
+        get_haadf(hs.load(str(f))) for f in Path(folder).iterdir() if f.is_file()
+    ]
+    images = [s.data for s in signals]
+    angles = [
+        float(s.original_metadata.Scan.ScanRotation) * 180 / np.pi for s in signals
+    ]
+
+
+elif test_dataset == "B":
+    from scipy.misc import face, ascent
+    from scipy.ndimage import rotate
+
     img1 = face(True)  # [:,:768]
-    img1 = ascent()
     img1 = np.pad(img1, 200, "constant")
     SHIFT = [80, 90]
     ANGLE = 45
@@ -41,66 +72,102 @@ if True:
     images = [img1, img2]
     angles = (0, ANGLE)
 
+elif test_dataset == "C":
+    import h5py
+
+    file = (
+        "../data_examples/nonlinear_drift_correction_synthetic_dataset_for_testing.mat"
+    )
+    f = h5py.File(file, mode="r")
+    img1 = f["image00deg"][:]
+    img2 = f["image90deg"][:]
+    img3 = f["imageIdeal"][:]
+    images = [img1, img2]
+    angles = [0, 90]
+
+else:
+    print("Specified wrong data?")
+
 images = [normalise_max(img) for img in images]
-padded_images, weights = pad_images(images)
+padded_images, weights = pad_images(images, pad_scale=pad_scale)
 
-data_raw = transform(padded_images, angles, weights=1)
-data_raw[1] = data_raw[1].swapaxes(0, 1)
-data_raw = data_raw.reshape((2, -1) + padded_images[0].shape)
-data_raw = data_raw.swapaxes(0, 1)
+# Set the various sheares, scales, first in terms of range
+sheares, scales = set_shear_and_scale_ranges(
+    padded_images[0].shape, shear_steps=shear_steps, scale_steps=scale_steps, pix=1
+)
+# Then in terms of transform matrices
+transform_matrices = set_transform_matrices(angles, sheares, scales)
+rotation_matrices, shear_matrices, scale_matrices = transform_matrices
 
-data = transform(padded_images, angles, weights=weights)
-data[1] = data[1].swapaxes(0, 1)
-data = data.reshape((2, -1) + padded_images[0].shape)
-# s1, s2 = [hs.signals.Signal2D(d) for d in data]
+# Scale and shear the masked data
+data = transform(
+    padded_images, rotation_matrices, shear_matrices, scale_matrices, weights=weights
+)
+data = data.reshape((len(padded_images), -1) + padded_images[0].shape)
 data = data.swapaxes(0, 1)
 
-centre_index = (len(data) - 1) // 2
+correlations = []
+for i, image_sequence in enumerate(data):
+    img1 = image_sequence[0]
+    corr = [
+        correlation_function(fft2(weights * img1), fft2(weights * img2))
+        for img2 in image_sequence[1:]
+    ]
+    correlations.append(corr)
 
-o1, o2 = w.Output(), w.Output()
-if is_notebook():
-    with o1:
-        fig, ax1 = plt.subplots()
-        setsize(fig, 500)
-    with o2:
-        fig, ax2 = plt.subplots()
-        setsize(fig, 500)
-else:
-    fig, (ax1, ax2) = plt.subplots(ncols=2)
+correlations2 = np.array(correlations).swapaxes(0, 1)
 
-ax1.imshow(data_raw[centre_index, 0], cmap="viridis")
-ax2.imshow(data_raw[centre_index, 1], cmap="viridis")
-h = w.HBox([o1, o2])
-display(h)
+max_indexes = []
+shifts_list = []
+for corr2 in correlations2:
+    max_indexes.append(np.argmax([np.max(corr) for corr in corr2]))
+    shifts_list.append([translation_function(corr) for corr in corr2])
 
+# Plot masked images
+data2 = np.array(data).swapaxes(0, 1)
+image_sums = []
+for i, img_array in enumerate(data2[1:]):
+    max_index = max_indexes[i]
+    img1 = data2[0, max_index]
+    img2 = img_array[max_index]
+    img2_shifted = np.fft.ifftn(
+        fourier_shift(fft2(img2), shifts_list[i][max_index])
+    ).real
+    image_sums.append(img1 + img2_shifted)
 
-correlations = [
-    cross_correlation(fft2(weights * img1), fft2(weights * img2)) for img1, img2 in data
-]
+fig, AX = plt.subplots(ncols=len(padded_images) - 1, squeeze=False)
+for i, ax in enumerate(np.reshape(AX, np.prod(AX.shape))):
+    ax.imshow(image_sums[i], cmap="viridis")
+    ax.axis("off")
 
-max_index = np.argmax([np.max(corr) for corr in correlations])
-shifts = [translation_orig(corr) for corr in correlations]
-shifts[max_index]
+# Plot whole images
+fig, AX = plt.subplots(ncols=len(padded_images) - 1, squeeze=False)
 
-o1, o2 = w.Output(), w.Output()
-if is_notebook():
-    with o1:
-        fig, ax1 = plt.subplots()
-        setsize(fig)
-    with o2:
-        fig, ax2 = plt.subplots()
-        setsize(fig)
-else:
-    fig, (ax1, ax2) = plt.subplots(ncols=2)
+# Loop across 0, numImages - 1
+for i, ax in enumerate(np.reshape(AX, np.prod(AX.shape))):
+    shear_index, scale_index = np.unravel_index(
+        max_indexes[i], (shear_steps, scale_steps)
+    )
+    shear, scale = sheares[shear_index], scales[scale_index]
 
-img1, img2 = data[max_index]
-img2_shifted = np.fft.ifftn(fourier_shift(fft2(img2), shifts[max_index])).real
-ax1.imshow(img1 + img2_shifted, cmap="viridis")
+    angle = [angles[0]]  # first image and i+1 image
+    rot_matrices, shear_matrices, scale_matrices = set_transform_matrices(
+        [angle], [shear], [scales]
+    )
+    img1 = transform_single_image(
+        padded_images[0], rot_matrices[0], shear_matrices[0], scale_matrices[0]
+    )
 
-img1, img2 = data_raw[max_index]
-img2_shifted = np.fft.ifftn(fourier_shift(fft2(img2), shifts[max_index])).real
-ax2.imshow(img1 + img2_shifted, cmap="viridis")
-print(shifts[max_index])
+    angle = [angles[i + 1]]  # first image and i+1 image
+
+    rot_matrices, shear_matrices, scale_matrices = set_transform_matrices(
+        [angle], [shear], [scales]
+    )
+
+    img2 = transform_single_image(
+        padded_images[i + 1], rot_matrices[0], shear_matrices[0], scale_matrices[0]
+    )
+
+    ax.imshow(img1 + img2, cmap="viridis")
+    ax.axis("off")
 plt.show()
-h = w.HBox([o1, o2])
-display(h)
--- python/transforms.py
+++ python/transforms.py
@@ -1,20 +1,14 @@
-import matplotlib.pyplot as plt
-
-plt.rcParams["figure.max_open_warning"] = 2000
-
-import h5py
 import numpy as np
 from numpy.fft import fft2, ifft2
+import matplotlib.pyplot as plt
 
-from skimage.transform import rotate
-from skimage.feature import register_translation
+from skimage.transform import AffineTransform, warp
 from scipy.ndimage import fourier_shift
 from scipy.signal import fftconvolve
 
-from skimage.transform import AffineTransform, warp
 
-
-def cross_correlation(img1_fft, img2_fft):
+def hybrid_correlation(img1_fft, img2_fft):
+    "Unsure if this actually is hybrid correlation according to Ophus"
     m = img1_fft * np.conj(img2_fft)
 
     magnitude = np.sqrt(np.abs(m))
@@ -23,23 +17,45 @@ def cross_correlation(img1_fft, img2_fft):
     return Icorr
 
 
-def translation(corr):
-    translation = np.unravel_index(corr.argmax(), corr.shape)
-    halfshape = np.array(corr.shape) / 2
-    translation = (translation[::-1] - halfshape) % halfshape
-    return translation
+def phase_correlation(img1_fft, img2_fft):
+    "A and B are FFTs of images"
+    C = img1_fft * np.conj(img2_fft)
+    return np.fft.ifft2(C / np.abs(C)).real
 
 
-def translation_orig(corr):
-    translation = np.array(np.unravel_index(corr.argmax(), corr.shape))
-    halfshape = np.array(corr.shape) / 2
+def cross_correlation(im1, im2):
+    # get rid of the averages, otherwise the results are not good
+    im1 -= np.mean(im1)
+    im2 -= np.mean(im2)
 
-    translation = (translation + halfshape) % halfshape - halfshape
-    return translation
+    # calculate the correlation image; note the flipping of the images
+    return fftconvolve(im1, im2[::-1, ::-1], mode="same")
+
+
+def changespace(x, maxval):
+    """Shift indices from [0, max] to [-max/2, max/2]
+    Handles both single values and numpy shape tuples"""
+    x = np.array(x)
+    maxval = np.array(maxval)
+    half = maxval / 2
+    return (x + half) % maxval - half
+
+
+def phase_translation(corr):
+    "Turn phase correlation array into shift x,y"
+    shift = np.unravel_index(corr.argmax(), corr.shape)
+
+    shift = changespace(shift, corr.shape)
+    return shift
+
+
+def cross_translation(corr):
+    "Turn phase correlation array into shift x,y"
+    shift = np.unravel_index(corr.argmax(), corr.shape)
+    return shift - np.array(corr.shape) / 2
 
 
 def pad_images(images, pad_scale=1.25):
-    pad_scale = 1.25
     old_shape = np.array(images[0].shape)
     new_shape = old_shape * pad_scale  # approx
     padding = ((new_shape - old_shape) / 2).astype(int)
@@ -49,81 +65,88 @@ def pad_images(images, pad_scale=1.25):
     ]
     new_shape = np.array(padded_images[0].shape)
 
-    TwoDHanningWindow = hanningLocal(old_shape[0]) * hanningLocal(old_shape[1]).T
-    weights = np.pad(TwoDHanningWindow, padding, mode="constant", constant_values=0)
+    window = HanningImage(old_shape[0]) * HanningImage(old_shape[1]).T
+    weights = np.pad(window, padding, mode="constant", constant_values=0)
     return padded_images, weights
 
 
-def transform(padded_images, angles, weights=1):
-    steps = 7
+def set_shear_and_scale_ranges(image_shape, shear_steps=5, scale_steps=5, pix=1):
+    "Turn a number of shear and scale steps into a range of - to +."
+    # Scale so `pix` pixels wider with each step
+    scale = (image_shape[1] + pix) / image_shape[1] - 1
+    # Shear by an angle equivalent of shearing `pix` pixels
 
+    half_steps = (scale_steps - 1) / 2
+    scale_limits = scale * half_steps
+    scales = 1 + np.linspace(-scale_limits, scale_limits, scale_steps)
+
+    half_steps = (shear_steps - 1) / 2
+    shear = np.arctan(pix / image_shape[1])  # maybe scale[0]?
+    shear_limits = shear * half_steps
+    sheares = np.linspace(-shear_limits, shear_limits, shear_steps)
+    return sheares, scales
+
+
+def set_transform_matrices(angles, sheares, scales):
+    rotation_matrices = [
+        AffineTransform(rotation=np.deg2rad(angle)) for angle in angles
+    ]
+    print(sheares)
+    shear_matrices = [AffineTransform(shear=shear) for shear in sheares]
+    scale_matrices = [AffineTransform(scale=(1, scale)) for scale in scales]
+
+    return rotation_matrices, shear_matrices, scale_matrices
+
+
+def transform(
+    padded_images, rotation_matrices, shear_matrices, scale_matrices, weights=1, steps=5
+):
+    """Scales, then shears, and then rotates images according the
+    angles. If no window is given as a weights argument, the function
+    operates directly on the raw images.
+    
+    """
     shape = padded_images[0].shape
+    # Get shifts so that we can shift the image by half the image
+    # size and then rotate about the centre instead of the corner
+    # (and then shift back afterwards)
     shift_x, shift_y = np.array(shape) / 2.0
     tf_shift = AffineTransform(translation=[-shift_y, -shift_x])
     tf_shift_inv = AffineTransform(translation=[shift_y, shift_x])
 
-    shear_steps = scale_steps = steps  # must be odd
-
-    pix = 1
-    # Scale so pix pixels wider with each step
-    scale = (shape[1] + pix) / shape[1] - 1
-    # Shear by pix pixels
-    shear = np.arctan(pix / shape[1])  # maybe scale[0]?
-
-    half_steps = (steps - 1) / 2
-    scale_limits = scale * half_steps
-    shear_limits = shear * half_steps
-    scales = 1 + np.linspace(-scale_limits, scale_limits, scale_steps)
-    sheares = np.linspace(-shear_limits, shear_limits, shear_steps)
-
-    data = np.zeros((2, len(sheares), len(scales)) + shape)
-    for i, (img, angle) in enumerate(zip(padded_images, angles)):
-        tf_rotate = AffineTransform(rotation=np.deg2rad(angle))
-        for j, shear in enumerate(sheares):
-            tf_shear = AffineTransform(shear=shear)
-            for k, scale in enumerate(scales):
-                tf_scale = AffineTransform(scale=(1, scale))
+    data = np.zeros(
+        (len(padded_images), len(shear_matrices), len(scale_matrices)) + shape
+    )
+    for i, (img, tf_rotate) in enumerate(zip(padded_images, rotation_matrices)):
+        for j, tf_shear in enumerate(shear_matrices):
+            for k, tf_scale in enumerate(scale_matrices):
                 data[i, j, k] = weights * warp(
                     img,
-                    (
-                        tf_shift + tf_scale + tf_shear + (tf_rotate + tf_shift_inv)
-                    ).inverse,
+                    (tf_shift + tf_scale + tf_shear + tf_rotate + tf_shift_inv).inverse,
                     order=0,
                 )
 
     return data
 
 
-def hanningLocal(N):
-    # Should this be 1, N+1? Or 0, N?
-    Dim1Window = np.sin(np.pi * np.arange(1, N + 1) / (N + 1)) ** 2
-    Dim2Window = Dim1Window[:, None]
-    return Dim2Window
+def transform_single_image(img, rot_matrix, shear_matrix, scale_matrix, weights=1):
+    shift_x, shift_y = np.array(img.shape) / 2.0
+    tf_shift = AffineTransform(translation=[-shift_y, -shift_x])
+    tf_shift_inv = AffineTransform(translation=[shift_y, shift_x])
+    breakpoint()
+    return weights * warp(
+        img,
+        (tf_shift + scale_matrix + shear_matrix + rot_matrix + tf_shift_inv).inverse,
+        order=0,
+    )
+
+
+def HanningImage(N):
+    "Return a 2D hanning window of size NxN "
+    han = np.hanning(N)
+    return han * han[:, None]
 
 
 def normalise_max(img):
+    "Use to normalise before correlation. Unsure if necessary or a good idea"
     return img / img.max()
-
-
-def setsize(fig, px=1000):
-    try:
-        px = str(px) + "px"
-        fig.canvas.layout.width = px
-        fig.canvas.layout.height = px
-    except:
-        pass
-
-def is_notebook():
-    try:
-        from IPython import get_ipython
-
-        if "IPKernelApp" not in get_ipython().config:  # pragma: no cover
-            raise ImportError("console")
-            return False
-        if "VSCODE_PID" in os.environ:  # pragma: no cover
-            raise ImportError("vscode")
-            return False
-    except:
-        return False
-    else:  # pragma: no cover
-        return True
